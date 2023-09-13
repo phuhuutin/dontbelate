e5f6ce7d98d1c8b7b8f86666959ea510b80d5598
phuhuutin <phuhuutin@gmail.com>
2023-09-12 22:44:04 -0700

    Set things up so I can run the whole microservice with docker-compose.yml

--- config-service/src/main/java/org/dontbelate/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/org/dontbelate/configservice/ConfigServiceApplication.java
@@ -2,12 +2,12 @@ package org.dontbelate.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableConfigServer
-
+@EnableDiscoveryClient
 public class ConfigServiceApplication {
 
     public static void main(String[] args) {
--- service-registry/src/main/java/org/dontbelate/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/org/dontbelate/serviceregistry/ServiceRegistryApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-@RefreshScope
+//@RefreshScope
 public class ServiceRegistryApplication {
 
     public static void main(String[] args) {
