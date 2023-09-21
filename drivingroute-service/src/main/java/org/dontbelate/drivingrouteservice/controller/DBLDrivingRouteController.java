41c9e4e4ddf29c24e602c4564ed62925af234fcc
phuhuutin <phuhuutin@gmail.com>
2023-09-21 16:55:50 -0700

    Edited deletebyID(), so it will also producing a message when a route is deleted with ActionEnume.Delete.

--- drivingroute-service/src/main/java/org/dontbelate/drivingrouteservice/controller/DBLDrivingRouteController.java
+++ drivingroute-service/src/main/java/org/dontbelate/drivingrouteservice/controller/DBLDrivingRouteController.java
@@ -1,26 +1,33 @@
 package org.dontbelate.drivingrouteservice.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.dontbelate.drivingrouteservice.dto.GoogleDistanceMatrixResponse;
+import org.dontbelate.drivingrouteservice.entity.ActionEnum;
 import org.dontbelate.drivingrouteservice.entity.DBLAddress;
 import org.dontbelate.drivingrouteservice.entity.DBLDrivingRoute;
+import org.dontbelate.drivingrouteservice.entity.DBLDrivingRouteChangeMessage;
 import org.dontbelate.drivingrouteservice.repository.DBLAddressRepository;
 import org.dontbelate.drivingrouteservice.repository.DBLDrivingRouteRepository;
 import org.dontbelate.drivingrouteservice.service.DrivingRouteService;
-import org.dontbelate.drivingrouteservice.service.LoadDummyData;
 import org.dontbelate.drivingrouteservice.service.TravelTimeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 @RestController
 @RequestMapping("api/routeservice")
 @RefreshScope
+@RequiredArgsConstructor
 public class DBLDrivingRouteController {
+    @Autowired
+    private final StreamBridge streamBridge;
     @Autowired
     private DrivingRouteService drivingRouteService;
     @Autowired
@@ -28,15 +35,18 @@ public class DBLDrivingRouteController {
     @Autowired
     private TravelTimeService travelTimeService;
 
-    @Value("${dontbelate.refreshCheck}")
-    private String reFresshConfigurationCheck;
+
+//    @Value("${dontbelate.refreshCheck}")
+//    private String reFresshConfigurationCheck;
     @Autowired
     private DBLDrivingRouteRepository dBLDrivingRouteRepository;
+    @Autowired
+    private  Function<String, String> sayHello;
 
-    @GetMapping("refreshCheck")
-    public String refreshCheck(){
-        return this.reFresshConfigurationCheck;
-    }
+//    @GetMapping("refreshCheck")
+//    public String refreshCheck(){
+//        return this.reFresshConfigurationCheck;
+//    }
 
     @PostMapping("add")
     public ResponseEntity<DBLDrivingRoute> saveDBLDrivingRoute(@RequestBody DBLDrivingRoute theRoute){
@@ -44,10 +54,10 @@ public class DBLDrivingRouteController {
         return new ResponseEntity<>(savedRoute, HttpStatus.CREATED);
     }
 
-    @GetMapping("hello")
-    public String hellofromDrivingRouteService(){
-
-        return "Hello from DrivingRouteService";
+    @GetMapping("hello/{message}")
+    public String hellofromDrivingRouteService(@PathVariable String message){
+        streamBridge.send("helloTopic",message);
+        return message;
 
     }
 
@@ -66,7 +76,11 @@ public class DBLDrivingRouteController {
     public ResponseEntity<String> deletebyID(@PathVariable Long id){
         try{
             if(dBLDrivingRouteRepository.existsById(id)){
+
                 dBLDrivingRouteRepository.deleteById(id);
+                DBLDrivingRouteChangeMessage newMessage = new DBLDrivingRouteChangeMessage(DBLDrivingRoute.class.getTypeName(),
+                        ActionEnum.DELETED.toString(), id);
+                streamBridge.send("helloTopic", MessageBuilder.withPayload(newMessage).build());
                 return ResponseEntity.ok("Item deleted successfully.");
             } else {
                 return ResponseEntity.notFound().build();
