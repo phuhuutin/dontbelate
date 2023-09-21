721aa0311e6bc9fa7393f3af312162d873936b73
phuhuutin <phuhuutin@gmail.com>
2023-09-21 16:57:32 -0700

    Added a Message Consumer that is listened to the same topic that routeChangeMessage.

--- user-service/src/main/java/org/dontbelate/userservice/UserServiceApplication.java
+++ user-service/src/main/java/org/dontbelate/userservice/UserServiceApplication.java
@@ -1,16 +1,35 @@
 package org.dontbelate.userservice;
 
+import org.dontbelate.userservice.dto.DBLDrivingRouteChangeMessage;
+import org.dontbelate.userservice.entity.DBLUser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Consumer;
+
 @RefreshScope
 @SpringBootApplication
 @EnableFeignClients
 public class UserServiceApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
+
+    @Bean
+    public Consumer<DBLDrivingRouteChangeMessage> receiveMessage(){
+        return routeChangeMessage -> {
+            System.out.println("Received: " + routeChangeMessage);
+        };
+    }
+
+
 
 }
