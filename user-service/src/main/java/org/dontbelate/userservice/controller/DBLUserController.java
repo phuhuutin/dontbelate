4a006c9994c25f34d6e5a631a4b2168f3b59e9a2
phuhuutin <phuhuutin@gmail.com>
2023-09-23 02:40:33 -0700

    Update getRoutebyId() to use redisDrivingRouteClient(check Redis first before getting from Database)

--- user-service/src/main/java/org/dontbelate/userservice/controller/DBLUserController.java
+++ user-service/src/main/java/org/dontbelate/userservice/controller/DBLUserController.java
@@ -1,31 +1,56 @@
 package org.dontbelate.userservice.controller;
 
-import org.dontbelate.userservice.dto.DBLDrivingRouteDTO;
+import org.dontbelate.userservice.dto.DBLDrivingRoute;
 import org.dontbelate.userservice.dto.DBLUserDTO;
 import org.dontbelate.userservice.entity.DBLUser;
+import org.dontbelate.userservice.repository.DBLUserRepository;
+import org.dontbelate.userservice.repository.RouteRedisRepository;
 import org.dontbelate.userservice.service.DrivingRouteClient;
-import org.dontbelate.userservice.service.LoadDummyData;
+import org.dontbelate.userservice.service.RedisDrivingRouteClient;
 import org.dontbelate.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("api/userservice")
 public class DBLUserController {
+
+    @Autowired
+    private RedisDrivingRouteClient redisDrivingRouteClient;
+    @Autowired
+    private RouteRedisRepository routeRedisRepository;
     @Autowired
      private UserService userService;
 
     @Autowired
     private DrivingRouteClient drivingRouteClient;
+
+
+    @GetMapping("testRedis/{id}")
+    public String testRedis(@PathVariable Long id){
+        DBLDrivingRoute route = drivingRouteClient.getRoute(id).getBody();
+        try{
+            routeRedisRepository.save(route);
+            Optional<DBLDrivingRoute> reroute = routeRedisRepository.findById(route.getId());
+            if(reroute.isPresent()){
+                System.out.println(reroute.get());
+            }
+            return "Successfully saved";
+        }catch (Exception e){
+            return "fail";
+        }
+    }
     @PostMapping("addUser")
     public ResponseEntity<DBLUser> saveUser(@RequestBody DBLUserDTO dblUserDTO){
         DBLUser savedUser = userService.saveUser(dblUserDTO);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
     @PostMapping("addRoute")
-    public ResponseEntity<DBLDrivingRouteDTO> saveRoute(@RequestBody DBLDrivingRouteDTO theRoute){
+    public ResponseEntity<DBLDrivingRoute> saveRoute(@RequestBody DBLDrivingRoute theRoute){
         return  drivingRouteClient.saveDrivingRoute(theRoute);
     }
     @GetMapping("/{userId}")
@@ -33,5 +58,11 @@ public class DBLUserController {
         return userService.getUserById(userId);
     }
 
+
+    @GetMapping("getRoute/{id}")
+    public ResponseEntity<DBLDrivingRoute> getRoutebyId(@PathVariable Long id){
+        return new ResponseEntity<>(redisDrivingRouteClient.getDBLDrivingRoute(id),HttpStatus.OK );
+
+    }
 
 }
