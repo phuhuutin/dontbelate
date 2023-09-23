6742a81f304fc65ac22d1f919592207ec3f020da
phuhuutin <phuhuutin@gmail.com>
2023-09-23 02:41:54 -0700

    Added /get/{id} from DrivingRoute-service

--- user-service/src/main/java/org/dontbelate/userservice/service/DrivingRouteClient.java
+++ user-service/src/main/java/org/dontbelate/userservice/service/DrivingRouteClient.java
@@ -1,9 +1,10 @@
 package org.dontbelate.userservice.service;
 
-import org.dontbelate.userservice.dto.DBLDrivingRouteDTO;
+import org.dontbelate.userservice.dto.DBLDrivingRoute;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "drivingroute-service")
@@ -11,5 +12,9 @@ public interface  DrivingRouteClient {
     @GetMapping("api/routeservice/hello")
     public String hellofromDrivingRouteService();
     @PostMapping("api/routeservice/add")
-    public ResponseEntity<DBLDrivingRouteDTO> saveDrivingRoute(DBLDrivingRouteDTO theRoute);
+    public ResponseEntity<DBLDrivingRoute> saveDrivingRoute(DBLDrivingRoute theRoute);
+
+    @GetMapping("api/routeservice/get/{id}")
+    public ResponseEntity<DBLDrivingRoute> getRoute( @PathVariable Long id);
+
 }
