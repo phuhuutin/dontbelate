0379cf6dd0114153d001740e344dcdf31de312cc
phuhuutin <phuhuutin@gmail.com>
2023-09-26 02:47:18 -0700

    Spring Data Redis with Jedi Configuration.

--- user-service/src/main/java/org/dontbelate/userservice/UserServiceApplication.java
+++ user-service/src/main/java/org/dontbelate/userservice/UserServiceApplication.java
@@ -1,21 +1,24 @@
 package org.dontbelate.userservice;
 
-import org.dontbelate.userservice.dto.DBLDrivingRouteChangeMessage;
-import org.dontbelate.userservice.entity.DBLUser;
+import org.dontbelate.userservice.service.ServiceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
-import java.util.function.Consumer;
 
 @RefreshScope
 @SpringBootApplication
 @EnableFeignClients
 public class UserServiceApplication {
+
 
     private static final Logger logger = LoggerFactory.getLogger(UserServiceApplication.class);
 
@@ -23,12 +26,27 @@ public class UserServiceApplication {
         SpringApplication.run(UserServiceApplication.class, args);
     }
 
+    @Autowired
+    private ServiceConfig serviceConfig;
+
     @Bean
-    public Consumer<DBLDrivingRouteChangeMessage> receiveMessage(){
-        return routeChangeMessage -> {
-            System.out.println("Received: " + routeChangeMessage);
-        };
+    JedisConnectionFactory jedisConnectionFactory() {
+        String hostname = serviceConfig.getRedisServer();
+        int port = Integer.parseInt(serviceConfig.getRedisPort());
+        RedisStandaloneConfiguration redisStandaloneConfiguration
+                = new RedisStandaloneConfiguration(hostname, port);
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
+
+    @Bean
+    public RedisTemplate<?,?> redisTemplate(JedisConnectionFactory jedisConnectionFactory ) {
+        RedisTemplate<byte[], byte[]> template = new RedisTemplate<byte[], byte[]>();
+        template.setConnectionFactory(jedisConnectionFactory);
+        return template;
+    }
+
+
+
 
 
 
