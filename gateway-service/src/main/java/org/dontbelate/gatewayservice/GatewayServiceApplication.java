04600851460230fa9f49d303e5d56b181869fe6a
phuhuutin <phuhuutin@gmail.com>
2023-09-26 03:03:21 -0700

    GatewayServiceApplication

---
+++ gateway-service/src/main/java/org/dontbelate/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,15 @@
+package org.dontbelate.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@RefreshScope
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+}
