0762c922fbe90f746d523bb84c1d712b421a32c1
phuhuutin <phuhuutin@gmail.com>
2023-10-10 01:54:23 -0700

    Added UserServiceClient to driving-service

---
+++ drivingroute-service/src/main/java/org/dontbelate/drivingrouteservice/service/UserServiceClient.java
@@ -0,0 +1,13 @@
+package org.dontbelate.drivingrouteservice.service;
+
+import org.dontbelate.drivingrouteservice.dto.DBLUser;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "user-service")
+public interface UserServiceClient {
+    @GetMapping("api/userservice/{userId}")
+    public ResponseEntity<DBLUser> getUserById(@PathVariable Long userId);
+}
